55266e7500d22580912a188e9c529617f871f5c7
Cropus <mr.flashmob@bk.ru>
2021-03-22 01:34:20 +0300

    Errors + tag template

--- js/location.js
+++ js/location.js
@@ -18,10 +18,6 @@ const geoError = (error) => {
 
 
 
-//                      FLAG FOR API PROBLEMS
-let problemsAPI = false;
-
-
 
 //                      FETCH-METHODS
 // Use only for current-section
@@ -29,49 +25,81 @@ const fetchByLocation = (latitude, longitude) => {
     fetch(`https://community-open-weather-map.p.rapidapi.com/forecast?units=metric&lat=${latitude}&lon=${longitude}`, {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "8704498e48msh1624788c735cb86p1d33c0jsn7dd12089f4f7",
+            "x-rapidapi-key": "294cdc2942msh00e9cefa5ffe8d7p1dd66ejsncbd442974b97",
             "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
         }
     })
-        .then(function (response) { return response.json(); })
+        .then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+                return res.json();
+            } else {
+                let error = new Error();
+                if (res.status >= 500 && res.status < 600) {
+                    alert("server error, " + res.status);
+                } else if (res.status >= 400 && res.status < 500) {
+                    alert("client error, " + res.status);
+                } else if (res.status >= 300 && res.status < 400) {
+                    alert("redirection, " + res.status);
+                }
+                error.response = res;
+                throw error;
+            }
+        })
         .then(function (data) {
-            // Flag for API connections problems
-            problemsAPI = false;
-            // Set current location weather-data
+            // Set regular location weather-data
             setCurrent(data);
         })
-        .catch(function () {
-            // Handle API problem
-            if (!problemsAPI) {
-                problemsAPI = true;
-                alert("Problems with API")
+        .catch ((e) => {
+            if (!navigator.onLine) {
+                alert("Please, check your network connection");
             }
+            console.log(e);
+            document.querySelector('h1').textContent = "Ошибка";
         });
+
 }
 // Use only for regular-section
 function fetchByCity (city) {
+    let ans = "";
     fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${city}&units=metric`, {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "8704498e48msh1624788c735cb86p1d33c0jsn7dd12089f4f7",
+            "x-rapidapi-key": "294cdc2942msh00e9cefa5ffe8d7p1dd66ejsncbd442974b97",
             "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
         }
     })
-        .then(function (response) { return response.json(); })
+        .then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+                return res.json();
+            } else {
+                let error = new Error();
+                if (res.status >= 500 && res.status < 600) {
+                    alert("server error, " + res.status);
+                } else if (res.status >= 400 && res.status < 500) {
+                    alert("client error, " + res.status);
+                } else if (res.status >= 300 && res.status < 400) {
+                    alert("redirection, " + res.status);
+                }
+                error.response = res;
+                throw error;
+            }
+        })
         .then(function (data) {
-            // Flag for API connections problems
-            problemsAPI = false;
             // Set regular location weather-data
             setRegular(city, data);
+            ans = data.id;
         })
-        .catch(function () {
-            if (!problemsAPI) {
-                problemsAPI = true;
-                alert("Problems with API")
-                document.getElementById(`li-${city}`).remove();
-                localStorage.removeItem(city);
+        .catch ((e) => {
+            if (!navigator.onLine) {
+                alert("Please, check your network connection");
             }
+            console.log(e);
+            if (document.getElementById(`li-${city}`) !== null) {
+                document.getElementById(`li-${city}`).remove();
+            }
+            localStorage.removeItem(city);
         });
+    return ans;
 }
 
 
@@ -103,7 +131,24 @@ function setRegular(city, data) {
 // Add template-container on page (and close-button for it)
 function add(value) {
     // Create template-container for regular city
-    document.querySelector(".reg-container").insertAdjacentHTML('beforeend', `<li class="fav" id="li-${value}">
+    if ('content' in document.createElement('template')) {
+        var template = document.querySelector('#template');
+        var container = document.querySelector('.reg-container');
+        var c = template.content;
+        c.querySelector('h3').textContent = value;
+        c.querySelector('button').id = `close-${value}`;
+        c.querySelector('span').id = `temp-${value}`;
+        c.querySelector('img').id = `img-${value}`;
+        c.querySelector('.fav').id = `li-${value}`;
+        let features = c.querySelectorAll('.feature-content');
+        features[0].id = `wind-${value}`;
+        features[1].id = `cloud-${value}`;
+        features[2].id = `pressure-${value}`;
+        features[3].id = `humidity-${value}`;
+        features[4].id = `cords-${value}`;
+        container.appendChild(c.cloneNode(true));
+    } else {
+        document.querySelector(".reg-container").insertAdjacentHTML('beforeend', `<li class="fav" id="li-${value}">
                 <h3>${value}</h3>
                 <button class="button close-button" id="close-${value}">X</button>
                 <span class="regular-temperature" id="temp-${value}">?&degC</span>
@@ -126,6 +171,7 @@ function add(value) {
                     </li>
                 </ul>
             </li>`);
+    }
     // Set close-button for template-container
     document.getElementById(`close-${value}`).onclick = () => {
         document.getElementById(`li-${value}`).remove();
@@ -147,11 +193,55 @@ document.querySelector(".mobile-ref").onclick = function () {
 // Set add-button for favorite-section
 document.querySelector(".add-button").onclick = function () {
     const value = document.querySelector(".search-type").value;
-    if (value !== "" && localStorage.getItem(value) === null) {
-        localStorage.setItem(value, value)
-        add(value);
-        fetchByCity(value);
+    // add(value);
+    // let id = fetchByCity(value);
+    // let flag = false;
+    // for (var i = 0; i < localStorage.length; i++) {
+    //     if (localStorage.getItem(localStorage.key(i)) === id) {
+    //         flag = true;
+    //     }
+    // }
+    // if (value !== "" && !flag) {
+    //     localStorage.setItem(value, id);
+    // } else {
+    //     document.getElementById(`li-${value}`).remove();
+    // }
+    let flag = false;
+    for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) === value) {
+            flag = true;
+        }
     }
+    if (value !== "" && !flag) {
+        add(value);
+        let id = fetchByCity(value);
+        localStorage.setItem(value, id);
+        // for (var i = 0; i < localStorage.length; i++) {
+        //     if (localStorage.getItem(localStorage.key(i)) === id) {
+        //         flag = true;
+        //         console.log(id);
+        //     }
+        // }
+        // if (!flag) {
+        //     localStorage.setItem(value, id);
+        // } else {
+        //     document.getElementById(`li-${value}`).remove();
+        // }
+    }
+    // if (value !== "") {
+    //     add(value);
+    //     let id = fetchByCity(value);
+    //     for (var i = 0; i < localStorage.length; i++) {
+    //         if (localStorage.getItem(localStorage.key(i)) === id) {
+    //             flag = true;
+    //         }
+    //     }
+    //     if (!flag) {
+    //         localStorage.setItem(value, id);
+    //     } else {
+    //         document.getElementById(`li-${value}`).remove();
+    //     }
+    // }
     document.querySelector(".search-type").value = "";
     return false;
 }
@@ -166,10 +256,10 @@ window.onload = () => {
 
     // Add favorite cities
     for (let i = localStorage.length - 1; i >= 0; i--) {
-        add(localStorage.getItem(localStorage.key(i)));
+        add(localStorage.key(i));
     }
     // Set weather-data on favorite cities
     for (let i = localStorage.length - 1; i >= 0; i--) {
-        setTimeout(fetchByCity, (localStorage.length - i) * 2000, localStorage.getItem(localStorage.key(i)));
+        setTimeout(fetchByCity, (localStorage.length - i) * 2000, localStorage.key(i));
     }
 }
